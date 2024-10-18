15bf0aca9b3de5fd4df282e875725f80f9bd2fb1
iruooghene <ebohiruooghene@gmail.com>
2024-10-18 15:45:12 +0100

    first commit

---
+++ src/test/java/africa/semicolon/NewcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
